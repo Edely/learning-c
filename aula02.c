20564e39bd4b279a7191fb4b198be9e7e3fb8eb9
Edely <edely.gomes@convergenceworks.com.br>
2018-10-30 11:00:41 -0300

    Aula 02; gitignore

---
+++ aula02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    /*
+        tipo nome;
+        char
+        int
+        float
+        double
+    */
+
+    char letra  = 'a';
+    char asc    = 101;
+    int nro     = 10;
+    float n     = 5.25;
+    double d    = 5.0e3;
+
+    printf("%i\n", nro);
+
+    system("pause");
+    return 0;
+}

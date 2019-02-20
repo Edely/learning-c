2dadf36d94f4d863c0390ee76c26d378a86c4ef4
Edely <edely.gomes@convergenceworks.com.br>
2019-02-20 14:11:09 -0300

    aula 27

---
+++ aula27/01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //declara array de int com 1 dimensão
+    int vet[5];
+
+    //declara array de float com 2 dimensões
+    int mat[5][5];
+
+    //declara array de double com 3 dimensões
+    double cub[5][5][5];
+
+    //declara array de int com 4 dimensões
+    double X[5][5][5][5];
+
+    system("pause");
+    return 0;
+}

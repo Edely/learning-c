b88747989880cc1aad2eb35b132f8183cdec3b3b
Edely <edely.gomes@convergenceworks.com.br>
2019-02-26 10:40:53 -0300

    AUla 49 - Funções - Array como parâmetro

---
+++ aula49/01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    Arrays são sempre passados "Por Referência"
+    para uma função
+
+    Isso evita a cópia desnecessária de grandes
+    quantidades de dados para outras áreas de
+    memória durante a chamada da função, o
+    que afetaria o desempenho do programa
+
+    system("pause");
+    return 0;
+}
---
+++ aula49/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Passando um array "Por Referência"
+
+    void imprime(int *m, int n);
+    void imprime(int m[], int n);
+    void imprime(int m[5], int n);
+
+    system("pause");
+    return 0;
+}

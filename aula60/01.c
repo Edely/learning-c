19c1f768561cc405cdbecd638e8e095547d984ab
Edely <edely.gomes@convergenceworks.com.br>
2019-02-28 11:59:49 -0300

    Aula 60 - Alocação Dinâmica

---
+++ aula60/01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    /* A linnguagem C permite alocar (reservar)
+    dinamicamente (em temop de execução) blocos 
+    de memórias utilizando ponteiros. A esse 
+    processo dá-se o nome de ALOCAÇÃO DINÂMICA. */ 
+        
+    system("pause");
+    return 0;
+}
---
+++ aula60/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    /* A linguagem C ANSI usa apenas 4 funções
+    para a alocação dinâmica, disponíveis na
+    biblioteca stdlib.h */ 
+    malloc
+    calloc
+    realloc
+    free
+
+    //Existe também o operador
+    sizeof
+        
+    system("pause");
+    return 0;
+}

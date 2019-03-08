8ae0cd851316d33ba27e1fb61a9ec017d7002016
Edely <edely.gomes@convergenceworks.com.br>
2019-03-08 12:01:12 -0300

    Aula 77 - fprintf

---
+++ aula77/01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    Nas últimas aulas vimos como ler e
+    escrever em arquivos caracteres, 
+    strings e até mesmo blocos de bytes.
+
+    A lgg C tb permite escrever
+    uma lista formatada de variáveis em
+    arquivo do mesmo modo como fazemos na tela
+    do computador com o função printf()
+
+    Para isso usamos a função fprintf()
+
+    system("pause");
+    return 0;
+}
---
+++ aula77/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Forma geral: printf()
+    printf("tipos de saída", variáveis);
+
+    //Forma geral: fprintf()
+    fprintf(FILE *f, "tipos de saída", variaveis);
+    
+    system("pause");
+    return 0;
+}

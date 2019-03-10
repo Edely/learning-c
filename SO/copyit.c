466dba04efa0f9c196f0efe27fd54d8043423598
edely <edely.gomes@gmail.com>
2019-03-10 11:14:28 -0300

    lendo e fechand os arquivos

--- SO/copyit.c
+++ SO/copyit.c
@@ -1,56 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char *argv[]){
 
- /*
- copyit ArquivoOrigem ArquivoDestino 
+    int original_file = open(argv[1], O_RDONLY);
+    int destiny_file = open(argv[2], O_CREAT, S_IWUSR);
+    
+    if(!(original_file > 0)){
+        printf("Aquivo original n abriu\n");
+        exit(1);
+    }
 
-
- O programa copyitdeve criar uma cópia de ArquivoOrigemcom o novo nome ArquivoDestino. 
- Após finalizar a cópia, copyit deve informar o número de bytes copiado e finalizar sem erros. 
- Por exemplo:copyit: foram copiados 35827 bytes do arquivo mata5820181.txt para o arquivo mata5820191.txt
- 
- Se a cópia demorar mais que um segundo, então a cada segundo o programa deverá emitir uma mensagem:
- 
- copyit: cópia em andamento ...
- copyit: cópia em andamento ...
- copyit: cópia em andamento ...
-
- Se o programa encontrar qualquer tipo de erro, o mesmo deve pararimediatamente e emitir uma mensagem que declare 
- o nome do programa, a operação que falhou e a razão da falha, terminando o programa com código de retorno 1. 
- 
- Por exemplo:
-
- copyit: Não pode abrir o arquivo mata5820181.txt: Permissão negada
- copyit: Não pode escrever o arquivo mata5820191.txt: Disco cheio
-
- Se o programa é invocado incorretamente, então o mesmo deve abortar imediatamente e exibir uma mensagem de 
- ajuda para o usuário, por exemplo:
- 
- copyit: Número incorreto de argumentosuse: copyit ArquivoOrigem ArquivoDestino
-
-
-
-Por fim, após as checagens de erro, o programa deve ser algo similar a:
-
-configure as mensagens periódicas
-abra o arquivo origem ou finalize com um erro
-crie o arquivo destino ou finalize com um erro
-    repita{
-            leia uma certa quantidade de dados do arquivo origem
-            se a leitura foi interrompida, tente novamente
-            se existiu um erro de leitura, finalize com um erro
-            se não existem mais dados, finalize o loop
-            escreva uma certa quantidade de dados no arquivo destino
-            se a escrita foi interrompida, tente novamente
-            se nem todos os dados foram escritos, finalize com um erro
-        }
-    feche ambos os arquivos
-    Imprima a mensagem de finalização bem-sucedida
-
- */
-
- system("pause");
- return 0;
+    if(!(destiny_file > 0)){
+        printf("Aquivo de destino n abriu\n");
+        exit(1);
+    }
+    
+    close(original_file);
+    close(destiny_file);
+    return 0;
 }

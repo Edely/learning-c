6f9471be8da3a08f827362ec5b4fa649a0b4cebc
edely <edely.gomes@gmail.com>
2019-03-10 11:44:14 -0300

    pausa para entender melhor

--- SO/copyit.c
+++ SO/copyit.c
@@ -4,11 +4,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#define BUFFER_SIZE 16
 
 int main(int argc, char *argv[]){
 
+    char buffer_leitura[BUFFER_SIZE];
+
+    int bufferChars;
     int original_file = open(argv[1], O_RDONLY);
-    int destiny_file = open(argv[2], O_CREAT, S_IWUSR);
+    int destiny_file = open(argv[2], O_CREAT, S_IWOTH);
     
     if(!(original_file > 0)){
         printf("Aquivo original n abriu\n");
@@ -19,6 +23,19 @@ int main(int argc, char *argv[]){
         printf("Aquivo de destino n abriu\n");
         exit(1);
     }
+
+    while (1) {
+        /* Normal case --- some number of bytes read. */
+        bufferChars = read(original_file, buffer_leitura, BUFFER_SIZE);
+        if ( bufferChars > 0) {
+            printf("%d\n", bufferChars);
+            /* Process the data */
+        }
+        else if (bufferChars == 0)   /* EOF reached. */
+            break;
+        else   /* bufferChars < 0 --- read failure. */
+            printf("Read failed.");
+   }
     
     close(original_file);
     close(destiny_file);

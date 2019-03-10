ce8610a5f82d81f51400d1ad309ab49fb20e83b9
edely <edely.gomes@gmail.com>
2019-03-10 10:08:13 -0300

    finazliza instrucoes

--- SO/insrucoes_copyit.c
+++ SO/insrucoes_copyit.c
@@ -3,7 +3,7 @@
 
 int main(){
 
- /*
+/*
  copyit ArquivoOrigem ArquivoDestino 
 
 
@@ -25,7 +25,29 @@ int main(){
  copyit: Não pode abrir o arquivo mata5820181.txt: Permissão negada
  copyit: Não pode escrever o arquivo mata5820191.txt: Disco cheio
 
- Se o programa é invocado incorretamente, então o mesmo deve abortar imediatamente e exibir uma mensagem de ajuda para o usuário, por exemplo:copyit: Número incorreto de argumentosuse: copyit ArquivoOrigem ArquivoDestino
+ Se o programa é invocado incorretamente, então o mesmo deve abortar imediatamente e exibir uma mensagem de 
+ ajuda para o usuário, por exemplo:
+ 
+ copyit: Número incorreto de argumentosuse: copyit ArquivoOrigem ArquivoDestino
+
+
+
+Por fim, após as checagens de erro, o programa deve ser algo similar a:
+
+configure as mensagens periódicas
+abra o arquivo origem ou finalize com um erro
+crie o arquivo destino ou finalize com um erro
+    repita{
+            leia uma certa quantidade de dados do arquivo origem
+            se a leitura foi interrompida, tente novamente
+            se existiu um erro de leitura, finalize com um erro
+            se não existem mais dados, finalize o loop
+            escreva uma certa quantidade de dados no arquivo destino
+            se a escrita foi interrompida, tente novamente
+            se nem todos os dados foram escritos, finalize com um erro
+        }
+    feche ambos os arquivos
+    Imprima a mensagem de finalização bem-sucedida
 
  */
 

d4ea720dab3bdc1fc63e275a61a594c61f0fe302
edely <edely.gomes@gmail.com>
2019-03-24 10:53:55 -0300

    backup my shell

--- SO/pratica02/backup_myshell.c
+++ SO/pratica02/backup_myshell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int runShell();
 
@@ -14,20 +15,24 @@ int runShell(){
 
     char str[4096];
     char *palavras[100];
+    int i = 0;
     FILE *arq;
     arq = fopen("myscript", "r");
     
     while(1){
         fflush(stdout);
         char *commands = fgets(str, 4096, arq);
+        char token;
         if(commands == NULL){
             printf("\nFim do arquivo\n");
             exit(1);
         }            
-        else{
+        else{            
+            printf("%d\n", i);
             printf("myshell>");
             printf("%s", str);
         }
+        i++;
     };
 
     fclose(arq);    
